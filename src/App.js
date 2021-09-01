ade628d87fd9ffffa3517849fa3b37193730ef5b
Singhdeepak93 <deepak01234567890rajput@gmail.com>
2021-09-01 22:42:11 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Login from './Login';
 function App() {
   return (
     <>
-    <Login/>
+   <Login/>
     </>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -2,22 +2,30 @@ import React, { useState } from 'react'
 import { Grid, Paper, Avatar, TextField, Button } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Alert from '@material-ui/lab/Alert';
-const Login = () => {
-   //create a function
-   const successfullmessageshowing = (Fullmessage) => {
-        setSucessMessage(Fullmessage);
-    }
-    const [successMeassage, setSucessMessage] = useState(false);
-    const Change = (() => {
-        //call function
-        successfullmessageshowing('data  submit')
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
+const Login = () => {
+    const [formData, setFormData] = useState([]);
+
+    const handleChangeData = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const [successMeassage, setSucessMessage] = useState(false);
+    const Change = ((event) => {
+            console.log('formData', formData)
+            event.preventDefault()
+            setSucessMessage('data submitted');
     });
     const ImpChange = ((event) => {
-        event.preventDefault();
         window.location.reload(false);
-
     })
+
     const paperStyle = { padding: 20, height: '90vh', width: 280, margin: "20px auto" }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const btnstyle = { margin: '8px 0' }
@@ -25,33 +33,40 @@ const Login = () => {
         <>
             <div>
                 {
-
                     <Alert severity="success" variant="filled"  >{successMeassage}</Alert>
-
                 }
             </div>
             <Grid>
-
-                <Paper elevation={10} style={paperStyle}>
+                <Paper elevation={5} style={paperStyle}>
                     <Grid align='center'>
                         <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
                         <h2>Login In</h2>
                     </Grid>
-                    <TextField label='Name' placeholder='Enter username' fullWidth required />
-                    <TextField label='Email' placeholder='Enter your email' type='Email' fullWidth required />
-                    <TextField label='Phone NO' placeholder='Enter your Phone no' type=' text' fullWidth required />
+                    <TextField label='Name' name='fullName' onChange={handleChangeData} placeholder='Enter username' fullWidth required />
+                    <TextField label='Email' name='email_id' onChange={handleChangeData} placeholder='Enter your email' type='email' fullWidth required />
+                    <TextField label='Phone NO' name='mobile' onChange={handleChangeData} placeholder='Enter your Phone no' type='number' fullWidth required />
 
-                    <select>
+                    <FormControl >
+                        <Select
+                            onChange={handleChangeData}
+                            displayEmpty
+                            name='hoby'
+                            inputProps={{ 'aria-label': 'Without label' }}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'Cricket'}>Cricket</MenuItem>
+                            <MenuItem value={'song'}>SOng </MenuItem>
+                            <MenuItem value={'footBall'}>foot ball</MenuItem>
+                        </Select>
+                        <FormHelperText>Without label</FormHelperText>
+                    </FormControl>
 
-                        <option required>Hobby</option>
-                        <option>Reading</option>
-                        <option>Travelling</option>
-                        <option>Writing</option>
-                    </select>
-                    <TextField label='AGE' placeholder='Enter your Age' type='number' min="18" max="80" fullWidth required />
-                    <TextField label="Address" placeholder="Address" multiline rows={2} rowsMax={4} fullWidth required />
-                    <TextField label="State" placeholder="State" type='text' fullWidth required />
-                    <TextField label="City" placeholder="city" type='text' fullWidth required />
+                    <TextField label='AGE' name='age' onChange={handleChangeData} placeholder='Enter your Age' type='number' min="18" max="80" fullWidth required />
+                    <TextField label="Address" name='address' onChange={handleChangeData} placeholder="Address" multiline rows={2} rowsMax={4} fullWidth required />
+                    <TextField label="State" name='state' onChange={handleChangeData} placeholder="State" type='text' fullWidth required />
+                    <TextField label="City" name='city' onChange={handleChangeData} placeholder="city" type='text' fullWidth required />
 
                     <Button type='submit' color='primary' variant="contained" style={btnstyle} onClick={Change} fullWidth>Sumbit</Button>
                     <Button type='reset' color='primary' variant="contained" style={btnstyle} onClick={ImpChange} fullWidth>Cancle</Button>
